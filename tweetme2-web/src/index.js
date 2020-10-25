f9be245f96543d6c41590b4ae4c7e9e0bbad7777
tquangdo <trangia61@gmail.com>
2020-10-26 01:32:15 +0900

    20201026 1:32

--- tweetme2-web/src/index.js
+++ tweetme2-web/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { TweetsComponent } from './tweets'
+// import App from './App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// )
+const e = React.createElement
+const tweetsEl = document.getElementById("tweetme2")
+if (tweetsEl) {
+  ReactDOM.render(
+    e(TweetsComponent, tweetsEl.dataset), tweetsEl)
+}
